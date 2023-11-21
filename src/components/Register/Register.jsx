61c786c966907cf5bf17d3550802a805539aaf86
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-21 13:34:18 -0300

    feat<Cadastro> buttons for ui interactive implemented

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Career from "./Career/Career";
 import Person from "./Person/Person";
+import Vacation from "./Vacation/Vacation";
+import Removal from "./Removal/Removal";
+import "./Register.scss";
 
 export default function Register() {
+  const [currentPage, setCurrentPage] = useState("Person");
+
+  const handleButtonClick = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div>
-      {/* <Person /> */}
+      <div>
+        <button className="button-register" onClick={() => handleButtonClick("Person")}>Pessoa</button>
+        <button className="button-register" onClick={() => handleButtonClick("Career")}>Carreira</button>
+        <button className="button-register" onClick={() => handleButtonClick("Vacation")}>Férias</button>
+        <button className="button-register" onClick={() => handleButtonClick("Removal")}>Afastamento</button>
+      </div>
+
+      {currentPage === "Person" && <Person />}
+      {currentPage === "Career" && <Career/>}
+      {currentPage === "Vacation" && <Vacation/>}
+      {currentPage === "Removal" && <Removal/>}
     </div>
   );
 }
