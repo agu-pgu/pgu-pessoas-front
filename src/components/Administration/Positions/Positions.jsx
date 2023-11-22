800ea60f1b91fca5646cfa940956e73096990ba6
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-22 19:44:59 -0300

    feat<administration> positions form created

--- src/components/Administration/Positions/Positions.jsx
+++ src/components/Administration/Positions/Positions.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Positions.scss";
+import { CreateError, createPersonSucess } from "../../../assets/js/Alerts";
+import { createPositions } from "../../../services/callsAdministration/CallsPositions";
 
 export default function Positions() {
+  const [cargo, setCargo] = useState("");
+  // const [cargoDescricao, setCargoDescricao] = useState("");
+
+  const handleSubmitForCreateCargo = async (e) => {
+    e.preventDefault();
+    const data = {
+      CADASTRAR: [
+        {
+          CARGO: {
+            cargo_nome: cargo,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await createPositions(data);
+      if (response.data.SUCESSO == true) {
+        createPersonSucess();
+      }
+    } catch (error) {
+      CreateError();
+    }
+  };
+
   return (
-    <div>Positions</div>
-  )
+    <div>
+      <div className="formulario-container">
+        <div className="form-scroll">
+          <form
+            className="formulario-container"
+            onSubmit={handleSubmitForCreateCargo}
+          >
+            <label className="form-label">*Cargo:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="cargo"
+              id="cargo"
+              value={cargo}
+              onChange={(event) => setCargo(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <button className="form-button-submit" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
