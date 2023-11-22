ca74663123a0451358f68326a89141d2e7206df4
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-22 19:32:01 -0300

    feat<administration> function form created

--- src/components/Administration/Functions/Functions.jsx
+++ src/components/Administration/Functions/Functions.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Functions.scss";
+import { CreateError, createPersonSucess } from "../../../assets/js/Alerts";
+import { createFunction } from "../../../services/callsAdministration/callsFuncion";
 
 export default function Functions() {
-  return (
-    <div>Functions</div>
-  )
-}
+  const [funcao, setFuncao] = useState("");
+  // const [funcaoDescricao, setFuncaoDescricao] = useState("");
 
+  const handleSubmitForCreateFuncao = async (e) => {
+    e.preventDefault();
+    const data = {
+      CADASTRAR: [
+        {
+          FUNCAO: {
+            funcao_nome: funcao,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await createFunction(data);
+      if (response.data.SUCESSO == true) {
+        createPersonSucess();
+      }
+    } catch (error) {
+      CreateError();
+    }
+  };
+
+  return (
+    <div>
+      <div className="formulario-container">
+        <div className="form-scroll">
+          <form
+            className="formulario-container"
+            onSubmit={handleSubmitForCreateFuncao}
+          >
+            <label className="form-label">*Função:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="funcao"
+              id="funcao"
+              value={funcao}
+              onChange={(event) => setFuncao(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <button className="form-button-submit" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
