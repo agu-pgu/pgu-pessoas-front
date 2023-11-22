0986ee1b2eaa103eb590216560a0c20ef71df8c1
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-22 19:06:21 -0300

    feat<Sector> Form for sector created

--- src/components/Administration/Sector/Sector.jsx
+++ src/components/Administration/Sector/Sector.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Sector.scss";
+import { CreateError, createPersonSucess } from "../../../assets/js/Alerts";
+import { createSector } from "../../../services/callsAdministration/callsSector";
 
 export default function Sector() {
+  const [setor, setSetor] = useState("");
+  // const [setorDescricao, setSetorDescricao] = useState("");
+  const [setorSigla, setSetorSigra] = useState("");
+
+  const handleSubmitForCreateSetor = async (e) => {
+    e.preventDefault();
+    const data = {
+      CADASTRAR: [
+        {
+          SETOR: {
+            setor_nome: setor,
+            setor_sigla: setorSigla,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await createSector(data);
+      if (response.data.SUCESSO == true) {
+        createPersonSucess();
+      }
+    } catch (error) {
+      CreateError();
+    }
+  };
+
   return (
-    <div>Sector</div>
-  )
+    <div>
+      <div className="formulario-container">
+        <div className="form-scroll">
+          <form
+            className="formulario-container"
+            onSubmit={handleSubmitForCreateSetor}
+          >
+            <label className="form-label">*Setor:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="setor"
+              id="setor"
+              value={setor}
+              onChange={(event) => setSetor(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <label className="form-label">*Setor Sigla:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="setorSigla"
+              id="setorSigla"
+              value={setorSigla}
+              onChange={(event) => setSetorSigra(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <button className="form-button-submit" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/services/callsAdministration/callsSector.js
@@ -0,0 +1,23 @@
+import api from "../api";
+
+export const createSector = async (data) => {
+    const token = localStorage.getItem("token");
+    const uid = localStorage.getItem("uid");
+    
+    try {
+      const response = await api.post("/CadastrarSetor", data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          uid: uid,
+        },
+      });
+      if (response.data.SUCESSO === true) {
+        return response;
+      } else {
+        return 401;
+      }
+    } catch (error) {
+      return 500;
+    }
+  };
