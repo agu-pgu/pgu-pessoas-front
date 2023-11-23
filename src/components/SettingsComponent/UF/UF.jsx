03dfc56af5f05d810e7bde659376b891252a0db9
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-23 16:09:03 -0300

    feat<Settings> UF Form created

--- src/components/SettingsComponent/UF/UF.jsx
+++ src/components/SettingsComponent/UF/UF.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CreateError, createPersonSucess } from "../../../assets/js/Alerts";
+import "./UF.scss";
+import { createUF } from "../../../services/callsSettings/callsUF";
 
 export default function UF() {
+  const [uf, setuf] = useState("");
+  const [ufSigla, setUfSigla] = useState("");
+  const [ufRegiao, setUfRegiao] = useState("");
+
+  const handleSubmitForCreateUf = async (e) => {
+    e.preventDefault();
+    const data = {
+      CADASTRAR: [
+        {
+          UF: {
+            uf_nome: uf,
+            uf_sigla: ufSigla,
+            uf_regiao: ufRegiao,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await createUF(data);
+      if (response.data.SUCESSO == true) {
+        createPersonSucess();
+      }
+    } catch (error) {
+      CreateError();
+    }
+  };
+
   return (
-    <div>UF</div>
-  )
+    <div>
+      <div className="formulario-container">
+        <div className="form-scroll">
+          <form
+            className="formulario-container"
+            onSubmit={handleSubmitForCreateUf}
+          >
+            <label className="form-label">*UF:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="uf"
+              id="uf"
+              value={uf}
+              onChange={(event) => setuf(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <label className="form-label">*UF Sigla:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="ufSigla"
+              id="ufSigla"
+              value={ufSigla}
+              onChange={(event) => setUfSigla(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <label className="form-label">*UF Região:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="ufRegiao"
+              id="ufRegiao"
+              value={ufRegiao}
+              onChange={(event) => setUfRegiao(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <button className="form-button-submit" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
