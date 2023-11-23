3511f8d51be58517496b0e973a168a7abd7595b4
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-22 21:07:18 -0300

    feat<Administration> Unit form created

--- src/components/Administration/Unit/Unit.jsx
+++ src/components/Administration/Unit/Unit.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CreateError, createPersonSucess } from "../../../assets/js/Alerts";
+import { createUnit } from "../../../services/callsAdministration/callsUnit";
 
 export default function Unit() {
+  const [unidade, setUnidade] = useState("");
+  // const [unidadeDescricao, setUnidadeDescricao] = useState("");
+  const [unidadeSigla, setUnidadeSigla] = useState("");
+
+  const handleSubmitForCreateUnidade = async (e) => {
+    e.preventDefault();
+    const data = {
+      CADASTRAR: [
+        {
+          UNIDADE: {
+            unidade_nome: unidade,
+            unidade_sigla: unidadeSigla,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await createUnit(data);
+      if (response.data.SUCESSO == true) {
+        createPersonSucess();
+      }
+    } catch (error) {
+      CreateError();
+    }
+  };
+
   return (
-    <div>Unit</div>
-  )
+    <div>
+      <div className="formulario-container">
+        <div className="form-scroll">
+          <form
+            className="formulario-container"
+            onSubmit={handleSubmitForCreateUnidade}
+          >
+            <label className="form-label">*Unidade:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="unidade"
+              id="unidade"
+              value={unidade}
+              onChange={(event) => setUnidade(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <label className="form-label">*Unidade Sigla:</label>
+            <input
+              className="form-input"
+              type="text"
+              name="unidadeSigla"
+              id="unidadeSigla"
+              value={unidadeSigla}
+              onChange={(event) => setUnidadeSigla(event.target.value)}
+              maxLength={255}
+              required
+            />
+            <button className="form-button-submit" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/services/callsAdministration/callsUnit.js
@@ -0,0 +1,23 @@
+import api from "../api";
+
+export const createUnit = async (data) => {
+    const token = localStorage.getItem("token");
+    const uid = localStorage.getItem("uid");
+    
+    try {
+      const response = await api.post("/CadastrarUnidade", data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          uid: uid,
+        },
+      });
+      if (response.data.SUCESSO === true) {
+        return response;
+      } else {
+        return 401;
+      }
+    } catch (error) {
+      return 500;
+    }
+  };
