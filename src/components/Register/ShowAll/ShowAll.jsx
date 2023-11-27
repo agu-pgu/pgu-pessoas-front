18d3e307a8313d078b552448c27c4960610594f4
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-27 12:10:54 -0300

    feat<ShowAll> method to call getPessoa in table created

--- src/components/Register/ShowAll/ShowAll.jsx
+++ src/components/Register/ShowAll/ShowAll.jsx
@@ -1,40 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ShowAll.scss";
 import ShowPerson from "./ShowPerson/ShowPerson";
+import { getPessoa } from "../../../services/CallsPerson/callsShowAll";
 
 export default function ShowAll() {
   const [selectedList, setSelectedList] = useState(null);
+  const [peopleList, setPeopleList] = useState([]);
+
+  useEffect(() => {
+    const getPavimentaPessoa = async () => {
+      try {
+        const response = await getPessoa();
+        const pessoaData = response.data.RETORNO[0].RETORNO;
+
+        const formattedPeopleData = pessoaData.map((pessoa) => ({
+          name: pessoa.PESSOA.pessoa_nome || "Nome não disponível",
+          gender:
+            pessoa.PESSOA.genero_id[0]?.GENERO.genero_nome ||
+            "Gênero não disponível",
+          municipality:
+            pessoa.PESSOA.municipio_id[0]?.MUNICIPIO.municipio_nome ||
+            "Município não disponível",
+          UF:
+            pessoa.PESSOA.municipio_id[0]?.MUNICIPIO.uf_id[0]?.UF.uf_sigla ||
+            "UF não disponível",
+          birthDate:
+            pessoa.PESSOA.pessoa_data_nascimento ||
+            "Data de Nascimento não disponível",
+          cpf: pessoa.PESSOA.pessoa_cpf || "CPF não disponível",
+          siape: pessoa.PESSOA.pessoa_siape || "SIAPE não disponível",
+        }));
+
+        setPeopleList(formattedPeopleData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPavimentaPessoa();
+  }, []);
 
   const lists = {
-    Pessoa: [
-      {
-        name: "Person 1",
-        gender: "Male",
-        municipality: "City 1",
-        UF: "SP",
-        birthDate: "01/01/1990",
-        cpf: "123.456.789-00",
-        siape: "1234567",
-      },
-      {
-        name: "Person 2",
-        gender: "Female",
-        municipality: "City 2",
-        UF: "RJ",
-        birthDate: "02/02/1995",
-        cpf: "987.654.321-00",
-        siape: "7654321",
-      },
-      {
-        name: "Person 3",
-        gender: "Other",
-        municipality: "City 3",
-        UF: "MG",
-        birthDate: "03/03/1985",
-        cpf: "111.222.333-44",
-        siape: "9876543",
-      },
-    ],
+    Pessoa: peopleList,
     career: ["Job 1", "Job 2", "Job 3"],
     vacations: ["Vacation 1", "Vacation 2", "Vacation 3"],
     leave: ["Leave 1", "Leave 2", "Leave 3"],
