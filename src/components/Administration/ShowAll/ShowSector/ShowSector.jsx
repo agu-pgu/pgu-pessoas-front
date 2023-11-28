9bf52b0732593876ab330a1c8db5b8fb7d699d58
Joao Leite <joao.lsouza@agu.gov.br>
2023-11-28 01:32:02 -0300

    style<showSector> width changed

--- src/components/Administration/ShowAll/ShowSector/ShowSector.jsx
+++ src/components/Administration/ShowAll/ShowSector/ShowSector.jsx
@@ -3,21 +3,21 @@ import "./ShowSector.scss";
 
 export default function ShowSector({ peopleList }) {
   return (
-    <div className="scroll-container">
-      <table className="list-container-table">
-        <thead className="list-container-thead">
-          <tr className="list-container-tr">
-            <th className="list-container-th">Sigla</th>
-            <th className="list-container-th">Nome</th>
-            <th className="list-container-th">Descrição</th>
+    <div className="scroll-container-sector">
+      <table className="list-container-table-sector">
+        <thead className="list-container-thead-sector">
+          <tr className="list-container-tr-sector">
+            <th className="list-container-th-sector">Sigla</th>
+            <th className="list-container-th-sector">Nome</th>
+            <th className="list-container-th-sector">Descrição</th>
           </tr>
         </thead>
         <tbody>
           {peopleList.map((setor, index) => (
-            <tr key={index} className="list-container-tr">
-              <td className="list-container-td">{setor.sigla}</td>
-              <td className="list-container-td">{setor.nome}</td>
-              <td className="list-container-td">{setor.descricao}</td>
+            <tr key={index} className="list-container-tr-sector">
+              <td className="list-container-td-sector">{setor.sigla}</td>
+              <td className="list-container-td-sector">{setor.nome}</td>
+              <td className="list-container-td-sector">{setor.descricao}</td>
             </tr>
           ))}
         </tbody>
